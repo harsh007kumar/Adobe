d8326a1f8d66fb73deb7544dc6b029a2963837e7
Harsh via Mac <Harsh007kumar@gmail.com>
2018-02-03 00:27:30 +0530

    Initial commit

---
+++ CountSetBitsInAnInteger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountSetBitsInAnInteger
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CountSetBitsInAInteger(65455);
+            Console.ReadLine();
+        }
+        static void CountSetBitsInAInteger(int no)
+        {
+            int count = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if ((no & (1 << i)) != 0)
+                { count++; }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ ReverseWordsInAGivenString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseWordsInAGivenString
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string str = "harsh kumar";
+            ReverseWordsInAGivenString(str);
+            Console.ReadLine();
+        }
+
+        private static void ReverseWordsInAGivenString(string str)
+        {
+            for (int i = str.Length - 1; i >= 0; i--)
+                Console.Write(str[i]);
+        }
+    }
+}
